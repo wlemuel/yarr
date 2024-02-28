af12c69af2fab64a38ae43be9978b278bd2e3a75
Steve Lemuel <wlemuel@hotmail.com>
2024-02-28 12:51:35 +0000

    fix: use turso

--- go.mod
+++ go.mod
@@ -1,9 +1,17 @@
 module github.com/nkanaev/yarr
 
-go 1.16
+go 1.21
 
 require (
-	github.com/mattn/go-sqlite3 v1.14.7
-	golang.org/x/net v0.8.0
-	golang.org/x/sys v0.6.0
+	github.com/tursodatabase/libsql-client-go v0.0.0-20240220085343-4ae0eb9d0898
+	golang.org/x/net v0.21.0
+	golang.org/x/sys v0.17.0
+)
+
+require (
+	github.com/antlr/antlr4/runtime/Go/antlr/v4 v4.0.0-20230512164433-5d1fd1a340c9 // indirect
+	github.com/libsql/sqlite-antlr4-parser v0.0.0-20230802215326-5cb5bb604475 // indirect
+	golang.org/x/exp v0.0.0-20240222234643-814bf88cf225 // indirect
+	golang.org/x/text v0.14.0 // indirect
+	nhooyr.io/websocket v1.8.10 // indirect
 )
--- src/storage/storage.go
+++ src/storage/storage.go
@@ -2,7 +2,11 @@ package storage
 
 import (
 	"database/sql"
-	_ "github.com/mattn/go-sqlite3"
+	"fmt"
+	"log"
+	"os"
+
+	_ "github.com/tursodatabase/libsql-client-go/libsql"
 )
 
 type Storage struct {
@@ -10,13 +14,26 @@ type Storage struct {
 }
 
 func New(path string) (*Storage, error) {
-	db, err := sql.Open("sqlite3", path)
+	primaryUrl := os.Getenv("TURSO_URL")
+	if primaryUrl == "" {
+		return nil, fmt.Errorf("TURSO_URL environment variable not set")
+	}
+	authToken := os.Getenv("TURSO_AUTH_TOKEN")
+	if authToken == "" {
+		return nil, fmt.Errorf("TURSO_AUTH_TOKEN environment variable not set")
+	}
+
+	url := fmt.Sprintf("libsql://%s?authToken=%s", primaryUrl, authToken)
+
+	log.Printf("use turso: %s", primaryUrl)
+
+	db, err := sql.Open("libsql", url)
 	if err != nil {
 		return nil, err
 	}
 
 	// TODO: https://foxcpp.dev/articles/the-right-way-to-use-go-sqlite3
-	db.SetMaxOpenConns(1)
+	// db.SetMaxOpenConns(1)
 
 	if err = migrate(db); err != nil {
 		return nil, err
